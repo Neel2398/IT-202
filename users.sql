775ad9a2d392af159246bb33138b867fb9260804
Neel2398 <nkp48@njit.edu>
2019-11-01 08:52:45 -0400

    adding users.sql

---
+++ users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(80) NOT NULL,
+  `password` varchar(80) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+   INSERT INTO `users` (`id`, `username`, `password`) VALUES
+   (1, 'Neel', '2398'), (1, 'jay', '9876')
